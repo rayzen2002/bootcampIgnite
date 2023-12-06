c7313ba2a69f31597c96dce23d66763c69f396ca
rayzen2002 <emanuelzsmj@gmail.com>
2023-12-06 15:41:06 -0300

    Changing paste structure

--- node/02-api-rest-nodejs/knexfile.ts
+++
@@ -1,5 +0,0 @@
-import { config } from './src/database'
-
-// Update with your config settings.
-
-export default config
--- node/02-api-rest-nodejs/src/app.ts
+++
@@ -1,9 +0,0 @@
-import fastify from 'fastify'
-import { transactionsRoutes } from './routes/transactions'
-import cookie from '@fastify/cookie'
-
-export const server = fastify()
-server.register(cookie)
-server.register(transactionsRoutes, {
-  prefix: 'transactions',
-})
