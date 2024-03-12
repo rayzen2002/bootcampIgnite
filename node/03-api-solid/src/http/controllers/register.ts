c70bacde58d4fbf643974130f0c868fb11b449cc
rayzen2002 <emanuelzsmj@gmail.com>
2024-03-11 22:00:50 -0300

    UseCases and design patterns

---
+++ node/03-api-solid/src/http/controllers/register.ts
@@ -0,0 +1,30 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { RegisterService } from '@/services/register'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/services/errors/user-already-exists'
+
+export async function register(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+  const { name, email, password } = registerBodySchema.parse(request.body)
+  try {
+    const userRepository = new PrismaUsersRepository()
+    const registerService = new RegisterService(userRepository)
+    await registerService.execute({
+      name,
+      email,
+      password,
+    })
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message })
+    }
+    throw err
+  }
+
+  return reply.status(201).send()
+}
