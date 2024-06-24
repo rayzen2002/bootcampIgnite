6d1ce0044c35790581fa80a1f14b7c8e794aa220
rayzen2002 <emanuelzsmj@gmail.com>
2024-06-24 19:11:23 -0300

    Controllers & Testes E2E

--- node/03-api-solid/src/http/controllers/authenticate.ts
+++ node/03-api-solid/src/http/controllers/authenticate.ts
@@ -16,16 +16,23 @@ export async function authenticate(
   try {
     const userRepository = new PrismaUsersRepository()
     const authenticateService = new AuthenticateService(userRepository)
-    await authenticateService.execute({
+    const { user } = await authenticateService.execute({
       email,
       password,
     })
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      },
+    )
+    return reply.status(200).send(token)
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({ message: err.message })
     }
     throw err
   }
-
-  return reply.status(200).send()
 }
