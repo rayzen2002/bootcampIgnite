5304b53f46643ef75716406952f7bfb4335dd439
rayzen2002 <emanuelzsmj@gmail.com>
2024-06-25 10:30:36 -0300

    e2e Register test

--- node/03-api-solid/src/http/controllers/register.spec.ts
+++ node/03-api-solid/src/http/controllers/register.spec.ts
@@ -1,2 +1,20 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
 test('ok', () => {})
+describe('Register (e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('Should be able to Register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'john@email.com',
+      password: '123456',
+    })
+    expect(response.statusCode).toEqual(201)
+  })
+})
